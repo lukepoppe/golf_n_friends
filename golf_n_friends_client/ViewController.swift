712fe5ab266d070ea2cb42fe27fb6b7805b13e75
Luke Poppe <luke.poppe@kineticdata.com>
2016-05-03 16:14:51 -0500

    Initial Commit

---
+++ golf_n_friends_client/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  golf_n_friends_client
+//
+//  Created by Luke Poppe on 5/3/16.
+//  Copyright © 2016 Luke Poppe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
