3ebbe70a62718aa104c9ac22d01e94ab32e7fd39
Luke Poppe <luke.poppe@kineticdata.com>
2016-05-12 12:54:09 -0500

    userFollow working

--- golf_n_friends_client/Friend.swift
+++
@@ -1,9 +0,0 @@
-//
-//  Friend.swift
-//  golf_n_friends_client
-//
-//  Created by Luke Poppe on 5/5/16.
-//  Copyright © 2016 Luke Poppe. All rights reserved.
-//
-
-import Foundation
---
+++ golf_n_friends_client/FriendItem.swift
@@ -0,0 +1,16 @@
+//
+//  FriendIteam.swift
+//  golf_n_friends_client
+//
+//  Created by Luke Poppe on 5/9/16.
+//  Copyright © 2016 Luke Poppe. All rights reserved.
+//
+
+import Foundation
+
+
+struct FriendIteam {
+    
+    let name: String
+    
+}
